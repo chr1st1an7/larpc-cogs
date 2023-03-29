11ba82674e9030014810374212ccae07fb9a68e7
Mr Kitty <gbluedev@gmail.com>
2023-03-29 19:07:17 +0000

    ban req

--- staff.py
+++ staff.py
@@ -100,7 +100,7 @@ class Staff(commands.Cog):
         await ctx.send(embed=embed, components=[[accept_button, deny_button]])
         
         # Wait for a button click response
-        button_ctx: disnake.ComponentContext = await self.client.wait_for("button_click", check=lambda c: c.author_id == ctx.author_id)
+        button_ctx: disnake.ComponentContext = await self.client.wait_for("button_click", check=lambda c: c.author.id == ctx.author.id)
         
         
         # Check which button was clicked and respond accordingly
