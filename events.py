6446d98a341163d7fdf51a8d50d11967bba57167
chr1st1an7 <107936754+chr1st1an7@users.noreply.github.com>
2023-08-17 13:23:10 +0200

    bot fix

--- events.py
+++ events.py
@@ -117,6 +117,8 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.channel.id == 1141366263407452220:
+            if message.author == self.client.user:
+             return  # Ignore messages sent by the bot itself
             # Check if the message has attachments
             embed = None
             if message.attachments:
